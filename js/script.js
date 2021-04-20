a1af68fb823b1f1108e04f8622a51c7a07516492
MichaelRosa9 <michaelrosa9@gmail.com>
2021-04-20 18:03:01 +0200

    js forward function

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+  $(".carousel .arrow-left").click(previousImage);
+  $(".carousel .arrow-right").click(nextImage);
+
+
+  function nextImage(){
+    var active_img = $(".photos img.active");
+    var acctive_icon = $("nav i.active") ;
+
+    active_img.removeClass("active");
+    active_img.next(".photos img").addClass("active");
+    
+    acctive_icon.removeClass("active");
+    acctive_icon.next("nav i").addClass("active");
+
+    if(active_img.next(".photos img").length === 0){
+      $(".photos img").first().addClass("active");
+      $("nav i").first().addClass("active");
+    }
+  }
+ 
+
+  function previousImage(){
+    console.log("previous");
+  }
+
+});
